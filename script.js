2465532f89e8a0ff62cc2c05046460caf189a297
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-08-14 01:43:39 +0100

    create a function displayTouch

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ const displayElement = document.getElementById('calcul');
 const acButton = document.getElementById('reset');
 
 const cButton  = document.getElementById('clear');
+
+let txtContent;
+
+//création du fonction displayTouch pour affichée le numero ou digit appuiyer
+function displayTouch() {
+    numpadButtons.forEach(button => {
+        button.addEventListener('click', ()=>{
+            txtContent = button.textContent;
+            
+        inputElement.value += txtContent;
+        displayElement.innerHTML += txtContent;
+        } )
+    });
+}
