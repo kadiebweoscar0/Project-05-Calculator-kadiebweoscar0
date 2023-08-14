5ec31189bad79104722f472cd44c2334536d7d70
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-08-14 11:38:34 +0100

    add method preventDefault on each button for block the default behavior and add button addition

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// import { parentPort } from 'worker_threads';
 import{ calculate } from './calculator.js';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
@@ -15,7 +16,13 @@ const acButton = document.getElementById('reset');
 
 const cButton  = document.getElementById('clear');
 
-let txtContent;
+const percentageButton = document.getElementById('percentage');
+
+const plusButton = document.getElementById('plus');
+
+const equalsButton = document.getElementById('equals');
+
+let txtContent, contentCalcul = 0;
 
 //création du fonction displayTouch pour affichée le numero ou digit appuiyer
 function displayTouch() {
@@ -24,7 +31,7 @@ function displayTouch() {
             txtContent = button.textContent;
             
         inputElement.value += txtContent;
-        displayElement.innerHTML += txtContent;
+        // displayElement.innerHTML += txtContent;
         } )
     });
 }
@@ -32,17 +39,26 @@ function displayTouch() {
 //appel de la fontion displayInput
 displayTouch()
 
-//suppression de l'attribut type du boutton AC
-acButton.removeAttribute("type");
-
 //button AC pour toutes effacer et annuler toutes les operation
-acButton.addEventListener('click', () => {
+acButton.addEventListener('click', (event) => {
+    event.preventDefault();
     inputElement.value = '';
     displayElement.innerHTML = '';
   });
 
 //button C pour effacer le dernier élment contenu dans inputElement et displayElement
-cButton.addEventListener('click', () => {
+cButton.addEventListener('click', (event) => {
+    event.preventDefault();
     inputElement.value = inputElement.value.slice(0, -1);
     displayElement.innerHTML = displayElement.innerHTML.slice(0, -1);
   });
+
+  
+
+//button addition
+plusButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    displayElement.innerHTML += inputElement.value;
+    displayElement.innerHTML += '+';
+    inputElement.value = ''
+})
