39acb60b1ad5286353f8bc065904c2afe6d712e8
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-08-14 01:39:47 +0100

    select buttons on js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-{ calculate } import './calculator';
+import{ calculate } from './calculator.js';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+//select buttons
+
+
+const numpadButtons = document.querySelectorAll('.numpad');
+
+const inputElement = document.getElementById('input');
+
+const displayElement = document.getElementById('calcul');
+
+const acButton = document.getElementById('reset');
+
+const cButton  = document.getElementById('clear');
