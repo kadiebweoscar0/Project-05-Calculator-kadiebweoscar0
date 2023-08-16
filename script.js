98fdfb3dd208917a1a5dc467c250d43c44514be6
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-08-16 17:19:30 +0100

    modif button *(multiplication) and refactory is button

--- script.js
+++ script.js
@@ -96,16 +96,21 @@ else {
 //button multiplication
 timesButton.addEventListener('click', (event) => {
     event.preventDefault();
-    if (displayElement.textContent.includes("*") === false) {
-        displayElement.innerHTML += inputElement.value;
-        displayElement.innerHTML += '*';
-        inputElement.value = ''
+    if (displayElement.textContent.includes("-") === true || 
+    displayElement.textContent.includes("*") === true || 
+    displayElement.textContent.includes("/") === true || 
+    displayElement.textContent.includes("+") === true) {
+
+    displayElement.innerHTML = ''
+    displayElement.innerHTML += inputElement.value;
+    displayElement.innerHTML += '*';
+    inputElement.value = ''
     }
+
     else {
-        displayElement.innerHTML = ''
-        displayElement.innerHTML += inputElement.value;
-        displayElement.innerHTML += '*';
-        inputElement.value = ''
+    displayElement.innerHTML += inputElement.value;
+    displayElement.innerHTML += '*';
+    inputElement.value = ''
     }
 })
 
